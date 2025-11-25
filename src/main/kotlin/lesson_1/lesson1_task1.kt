8c4572f506c539836a333b3f215944b514428c3f
andrei <minombre.andrei@yandex.ru>
2025-11-25 17:38:21 +0400

    KS-1-1 Задача 1 по созданию переменных

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,7 +1,11 @@
 package org.example.lesson_1
 
 fun main() {
-    ptintln("Тестовое решение к уроку 1 задаче 1")
-    ptintln("Какие-то доработки")
+//    ptintln("Тестовое решение к уроку 1 задаче 1")
+//    ptintln("Какие-то доработки")
+    var numberOfOrders: Int = 75
+    val thanksForThePurchase: String = "Спасибо за вашу покупку в нашем магазине. Thank you very much! "
+    println(numberOfOrders)
+    println(thanksForThePurchase)
 
 }
