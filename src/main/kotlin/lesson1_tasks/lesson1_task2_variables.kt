8a4eb37baf6254184aa13866fcc761977e2f389d
andrei <minombre.andrei@yandex.ru>
2025-11-26 11:52:22 +0400

    KS-1-2-real-task-variables Задача 2 по созданию переменных:  Добавилось количество работников интернет магазина

---
+++ src/main/kotlin/lesson1_tasks/lesson1_task2_variables.kt
@@ -0,0 +1,13 @@
+package org.example.lesson1_tasks
+
+fun main() {
+    var numberOfOrdersnew: Int = 75
+    val thanksForThePurchasenew: String = "Спасибо за вашу покупку в нашем магазине. Thank you very much! "
+//    Почему-то кириллица при выводе в консоль отображается как вопросительные знаки, в настройках выбрана кодировка UTF-8
+//    var numberOfStoreEmployees: Int = 2000
+    println(numberOfOrdersnew)
+    println(thanksForThePurchasenew)
+//    println(numberOfStoreEmployees)
+    var numberOfStoreEmployees: Int = 1999
+    println(numberOfStoreEmployees)
+}
