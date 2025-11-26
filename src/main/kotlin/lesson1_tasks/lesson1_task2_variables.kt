8390026a72ca90517ee4c22d21eadbb4831c907b
andrei <minombre.andrei@yandex.ru>
2025-11-26 15:12:56 +0400

    KS-1-2-real-task-variables Задача 2 fix 1

--- src/main/kotlin/lesson1_tasks/lesson1_task2_variables.kt
+++ src/main/kotlin/lesson1_tasks/lesson1_task2_variables.kt
@@ -3,10 +3,9 @@ package org.example.lesson1_tasks
 fun main() {
     var numberOfOrdersnew: Int = 75
     val thanksForThePurchasenew: String = "Спасибо за вашу покупку в нашем магазине. Thank you very much! "
-//    Почему-то кириллица при выводе в консоль отображается как вопросительные знаки, в настройках выбрана кодировка UTF-8
-//    var numberOfStoreEmployees: Int = 2000
     println(numberOfOrdersnew)
     println(thanksForThePurchasenew)
+    var numberOfStoreEmployees: Int = 2000
 //    println(numberOfStoreEmployees)
     var numberOfStoreEmployees: Int = 1999
     println(numberOfStoreEmployees)
