df4bf2e7d6ab226f7ec64edd32ded7ec43c34a02
andrei <minombre.andrei@yandex.ru>
2025-11-27 11:38:08 +0400

    KS-1-2-real-task-variables Задача 2 fix 2

--- src/main/kotlin/lesson1_tasks/lesson1_task2_variables.kt
+++ src/main/kotlin/lesson1_tasks/lesson1_task2_variables.kt
@@ -3,10 +3,12 @@ package org.example.lesson1_tasks
 fun main() {
     var numberOfOrdersnew: Int = 75
     val thanksForThePurchasenew: String = "Спасибо за вашу покупку в нашем магазине. Thank you very much! "
+
     println(numberOfOrdersnew)
     println(thanksForThePurchasenew)
+
     var numberOfStoreEmployees: Int = 2000
 //    println(numberOfStoreEmployees)
-    var numberOfStoreEmployees: Int = 1999
+    numberOfStoreEmployees = 1999
     println(numberOfStoreEmployees)
 }
