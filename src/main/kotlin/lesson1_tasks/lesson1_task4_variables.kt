d36b9f6ddf215553a34972ccb2a9d14fdb15e8d4
andrei <minombre.andrei@yandex.ru>
2025-12-01 13:48:48 +0400

    KS-1-4

---
+++ src/main/kotlin/lesson1_tasks/lesson1_task4_variables.kt
@@ -0,0 +1,17 @@
+package org.example.lesson1_tasks
+
+fun main() {
+    var length: Long = 40_868_600_000
+    var age: Byte = 27
+    var partOfDay: Double = 0.075
+    var seconds: Short = 6480
+    var partOfYear: Double = 2.0547945205479453E-4
+    var apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
