28ef83d4b6a48b5d72afced245f8c7849cd374b4
andrei <minombre.andrei@yandex.ru>
2025-12-01 17:31:15 +0400

    KS-1-4 fix 1

--- src/main/kotlin/lesson1_tasks/lesson1_task4_variables.kt
+++ src/main/kotlin/lesson1_tasks/lesson1_task4_variables.kt
@@ -1,12 +1,12 @@
 package org.example.lesson1_tasks
 
 fun main() {
-    var length: Long = 40_868_600_000
-    var age: Byte = 27
-    var partOfDay: Double = 0.075
-    var seconds: Short = 6480
-    var partOfYear: Double = 2.0547945205479453E-4
-    var apogee: Int = 327000
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
 
     println(length)
     println(age)
