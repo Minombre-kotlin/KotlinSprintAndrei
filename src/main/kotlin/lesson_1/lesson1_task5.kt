84083d426fe457cde0fc9242f1e85d73d4873463
andrei <minombre.andrei@yandex.ru>
2025-12-25 11:14:47 +0400

    KS-1-5 fix1

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,27 @@
+package org.example.lesson_1
+
+fun main(){
+    //объявляю общее время в секундах. В задаче показано время 1:30:09
+    val totalNumberOfSeconds = 5409
+
+    //перевожу общее количество секунд в количество целых часов
+    val numberOfWholeHours = totalNumberOfSeconds /3600
+
+    //вычисляю остаток секунд после перевода в часы
+    val remainingSecondsAfterСonversionToHours = totalNumberOfSeconds % 3600
+
+    //перевожу общее количество секунд в количество целых минут
+    val numberOfWholeMinutes = remainingSecondsAfterСonversionToHours /60
+
+    //вычисляю остаток секунд после перевода в минуты
+    val remainingSecondsAfterСonversionToMinutes = totalNumberOfSeconds % 60
+
+    println(totalNumberOfSeconds)
+    println(numberOfWholeHours)
+    println(remainingSecondsAfterСonversionToHours)
+    println(numberOfWholeMinutes)
+    println(remainingSecondsAfterСonversionToMinutes)
+
+    //Вывожу в консоль время, проведенное в космосе в формате [часы:минуты:секунды]
+    println(String.format("%02d:%02d:%02d", numberOfWholeHours, numberOfWholeMinutes, remainingSecondsAfterСonversionToMinutes))
+}
