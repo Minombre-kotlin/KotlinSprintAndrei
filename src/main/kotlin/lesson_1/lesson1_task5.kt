cafc9665fdff6c2fab9e67b81570577994194756
Андрей <minombre.andrei@yandex.ru>
2025-12-27 00:08:48 +0400

    ks-1-5 fix2

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,27 +1,13 @@
 package org.example.lesson_1
 
-fun main(){
-    //объявляю общее время в секундах. В задаче показано время 1:30:09
-    val totalNumberOfSeconds = 5409
+fun main() {
 
-    //перевожу общее количество секунд в количество целых часов
-    val numberOfWholeHours = totalNumberOfSeconds /3600
-
-    //вычисляю остаток секунд после перевода в часы
+    val totalNumberOfSeconds = 6480
+    const val numberOfWholeHours = totalNumberOfSeconds / 3600
     val remainingSecondsAfterСonversionToHours = totalNumberOfSeconds % 3600
+    const val numberOfWholeMinutes = remainingSecondsAfterСonversionToHours / 60
 
-    //перевожу общее количество секунд в количество целых минут
-    val numberOfWholeMinutes = remainingSecondsAfterСonversionToHours /60
-
-    //вычисляю остаток секунд после перевода в минуты
     val remainingSecondsAfterСonversionToMinutes = totalNumberOfSeconds % 60
 
-    println(totalNumberOfSeconds)
-    println(numberOfWholeHours)
-    println(remainingSecondsAfterСonversionToHours)
-    println(numberOfWholeMinutes)
-    println(remainingSecondsAfterСonversionToMinutes)
-
-    //Вывожу в консоль время, проведённое в космосе в формате [часы:минуты:секунды]
-    println(String.format("%02d:%02d:%02d", numberOfWholeHours, numberOfWholeMinutes, remainingSecondsAfterСonversionToMinutes))
+    println("%02d:%02d:02d".format(numberOfWholeHours, numberOfWholeMinutes, remainingSecondsAfterСonversionToMinutes))
 }
