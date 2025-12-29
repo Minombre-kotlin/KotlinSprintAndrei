12c8205534ec0463ec95c0b646e8df4ad5db87cf
Андрей <minombre.andrei@yandex.ru>
2025-12-29 11:29:27 +0400

    KS-2-2 fix 1

--- src/main/kotlin/lesson2/task2.kt
+++ src/main/kotlin/lesson2/task2.kt
@@ -1,12 +1,18 @@
 package org.example.lesson2
 
 fun main() {
-    val salaryOfPermanentEmployees: Int = 30000 * 50
-    val generalExpensesAfterTheArrivalOfInterns: Int = salaryOfPermanentEmployees + 20000 * 30
+    val numberOfPermanentEmployees: Int = 50
+    val salaryOfPermanentEmployees: Int = 30000
+    val totalSalaryOfPermanentEmployees: Int = numberOfPermanentEmployees * salaryOfPermanentEmployees
+    val numberOfInterns: Int = 30
+    val salaryOfInterns: Int = 20000
+    val totalSalaryOfInterns: Int = numberOfInterns * salaryOfInterns
+    val totalExpensesAfterTheArrivalOfInterns: Int = salaryOfPermanentEmployees + totalSalaryOfInterns
+    val totalNumberOfAllEmployeesTogether: Int = numberOfPermanentEmployees + numberOfInterns
     val averageSalaryPerEmployeeAfterPlacementOfInterns: Int =
-        (salaryOfPermanentEmployees + generalExpensesAfterTheArrivalOfInterns) / 80
+        (salaryOfPermanentEmployees + totalExpensesAfterTheArrivalOfInterns) / totalNumberOfAllEmployeesTogether
 
-    println(salaryOfPermanentEmployees)
-    println(generalExpensesAfterTheArrivalOfInterns)
+    println(totalSalaryOfPermanentEmployees)
+    println(totalExpensesAfterTheArrivalOfInterns)
     println(averageSalaryPerEmployeeAfterPlacementOfInterns)
 }
