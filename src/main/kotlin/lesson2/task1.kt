22cc18728d0082d68e377e39210fbf55cff3ce45
Андрей <minombre.andrei@yandex.ru>
2026-01-05 23:35:12 +0400

    KS-2-2 fix 2

--- src/main/kotlin/lesson2/task1.kt
+++ src/main/kotlin/lesson2/task1.kt
@@ -2,7 +2,11 @@ package org.example.lesson2
 
 fun main() {
     val numberOfSchoolchildren: Int = 4
-    val totalPointsInEnglish: Int = 3 + 4 + 3 + 5
+    val pointFirst: Int = 3
+    val pointSecond: Int = 4
+    val pointThird: Int = 3
+    val pointFourth: Int = 5
+    val totalPointsInEnglish: Int = pointFirst + pointSecond + pointThird + pointFourth
     val averageEnglishPointsFloat: Float = totalPointsInEnglish.toFloat() / numberOfSchoolchildren
     println(averageEnglishPointsFloat)
 }
