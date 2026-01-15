965b87095f088266e4956f0fb553064b4762cf17
Андрей <minombre.andrei@yandex.ru>
2026-01-15 21:09:46 +0400

    KS-3-1

---
+++ src/main/kotlin/Lesson_3/KS-3-1.kt
@@ -0,0 +1,9 @@
+package org.example.Lesson_3
+
+fun main() {
+    val name: String = "Евгений"
+    val helloMorning: String = "Доброе утро, $name!"
+    println(helloMorning)
+    val helloAfternoon: String = "Добрый день, $name! "
+    println(helloAfternoon)
+}
