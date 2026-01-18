8eba778ff9dec6205743e1aaedb57d80da49ff67
Андрей <minombre.andrei@yandex.ru>
2026-01-18 14:14:39 +0400

    KS-2-5 удалил лишний файл KS-3-1 из ветки KS-2-5

--- src/main/kotlin/Lesson_3/KS-3-1.kt
+++
@@ -1,9 +0,0 @@
-package org.example.Lesson_3
-
-fun main() {
-    val name: String = "Евгений"
-    val helloMorning: String = "Доброе утро, $name!"
-    println(helloMorning)
-    val helloAfternoon: String = "Добрый день, $name! "
-    println(helloAfternoon)
-}
