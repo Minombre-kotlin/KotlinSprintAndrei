314b271e3fe7b1b979b06538650d35712925036f
Андрей <minombre.andrei@yandex.ru>
2026-01-18 13:41:45 +0400

    KS-2-3 fix

--- src/main/kotlin/lesson2/lesson 2 task 3.kt
+++ src/main/kotlin/lesson2/lesson 2 task 3.kt
@@ -3,11 +3,15 @@ package org.example.lesson2
 fun main() {
     val timeStartHours: Int = 9
     val timeStartMinutes: Int = 39
-    val timeSumHoursAndMinutesInMinutes: Int = timeStartHours * 60 + timeStartMinutes
+    val oneHourInMinutes: Int = 60
+    val timeSumHoursAndMinutesInMinutes: Int = timeStartHours * oneHourInMinutes + timeStartMinutes
+
     val timeTravel: Int = 457
+
     val timeArrivalInMinutes: Int = timeSumHoursAndMinutesInMinutes + timeTravel
-    val timeArrivalHours: Int = timeArrivalInMinutes / 60
-    val timeArrivalMinutes: Int = timeArrivalInMinutes % 60
+    val timeArrivalHours: Int = timeArrivalInMinutes / oneHourInMinutes
+    val timeArrivalMinutes: Int = timeArrivalInMinutes % oneHourInMinutes
     val timeArrivalInHoursCorrectFormat = String.format("%02d:%02d", timeArrivalHours, timeArrivalMinutes)
+
     println(timeArrivalInHoursCorrectFormat)
 }
