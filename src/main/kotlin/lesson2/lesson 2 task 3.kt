397c97f75b18cc277553b862b94168dab9ddaded
Андрей <minombre.andrei@yandex.ru>
2026-01-19 23:22:59 +0400

    KS-2-3 fix 3 Отформатировал нейминг константы

--- src/main/kotlin/lesson2/lesson 2 task 3.kt
+++ src/main/kotlin/lesson2/lesson 2 task 3.kt
@@ -1,17 +1,17 @@
 package org.example.lesson2
 
-const val oneHourInMinutes: Int = 60
+const val ONE_HOUR_IN_MINUTES: Int = 60
 
 fun main() {
     val timeStartHours: Int = 9
     val timeStartMinutes: Int = 39
-    val timeSumHoursAndMinutesInMinutes: Int = timeStartHours * oneHourInMinutes + timeStartMinutes
+    val timeSumHoursAndMinutesInMinutes: Int = timeStartHours * ONE_HOUR_IN_MINUTES + timeStartMinutes
 
     val timeTravel: Int = 457
 
     val timeArrivalInMinutes: Int = timeSumHoursAndMinutesInMinutes + timeTravel
-    val timeArrivalHours: Int = timeArrivalInMinutes / oneHourInMinutes
-    val timeArrivalMinutes: Int = timeArrivalInMinutes % oneHourInMinutes
+    val timeArrivalHours: Int = timeArrivalInMinutes / ONE_HOUR_IN_MINUTES
+    val timeArrivalMinutes: Int = timeArrivalInMinutes % ONE_HOUR_IN_MINUTES
     val timeArrivalInHoursCorrectFormat = String.format("%02d:%02d", timeArrivalHours, timeArrivalMinutes)
 
     println(timeArrivalInHoursCorrectFormat)
