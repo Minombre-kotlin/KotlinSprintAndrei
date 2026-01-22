20cf574749e7d184c4a5fc56eb34b078f87a3939
Андрей <minombre.andrei@yandex.ru>
2026-01-22 20:30:01 +0400

    KS-3-2

---
+++ src/main/kotlin/lesson3/KS-3-2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson3
+
+fun main() {
+    val nameMaiden: String = "Андреева Татьяна Сергеевна"
+    val nameAfterMarriage: String = "Сидорова Татьяна Сергеевна"
+
+    val ageBeforeMarriage: Int = 20
+    val ageAfterMarriage: Int = 22
+
+    println("ФИО до замужества: $nameMaiden, возраст до замужества: $ageBeforeMarriage лет")
+    println("ФИО после замужества: $nameAfterMarriage, возраст после замужества: $ageAfterMarriage лет")
+}
