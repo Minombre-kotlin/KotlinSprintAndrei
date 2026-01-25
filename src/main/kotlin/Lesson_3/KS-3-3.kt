7fb3f27fad3b7f7ff907ed073a7ada5644617d46
Андрей <minombre.andrei@yandex.ru>
2026-01-25 18:49:22 +0400

    KS-3-3 fix

--- src/main/kotlin/Lesson_3/KS-3-3.kt
+++ src/main/kotlin/Lesson_3/KS-3-3.kt
@@ -2,7 +2,18 @@ package org.example.Lesson_3
 
 fun main() {
     val number: Int = 2
-    for(factor in 1..9){
-        println("$number * $factor = ${number * factor}")
-    }
+    var factor: Int = 1
+    val table = """
+        $number * ${factor++} = ${number * (factor - 1)}
+        $number * ${factor++} = ${number * (factor - 1)}
+        $number * ${factor++} = ${number * (factor - 1)}
+        $number * ${factor++} = ${number * (factor - 1)}
+        $number * ${factor++} = ${number * (factor - 1)}
+        $number * ${factor++} = ${number * (factor - 1)}
+        $number * ${factor++} = ${number * (factor - 1)}
+        $number * ${factor++} = ${number * (factor - 1)}
+        $number * ${factor++} = ${number * (factor - 1)}
+        $number * ${factor++} = ${number * (factor - 1)}
+    """.trimIndent()
+    println(table)
 }
