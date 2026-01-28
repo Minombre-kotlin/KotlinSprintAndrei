a9c2635ee1a2f4b053e6d9e8327d6cadd4bf75dd
Андрей <minombre.andrei@yandex.ru>
2026-01-28 21:25:06 +0400

    KS-3-4 fix

--- src/main/kotlin/lesson3/KS-3-4.kt
+++ src/main/kotlin/lesson3/KS-3-4.kt
@@ -3,19 +3,15 @@ package org.example.lesson3
 fun main() {
     var whereFrom: String = "E2"
     var whereTo: String = "E4"
-    var moveNumber: String = "№1"
-    var lineToSend: String = """
-Игрок сделал ход из $whereFrom в $whereTo $moveNumber
-"""
+    var moveNumber: Int = 1
+    var lineToSend: String = "[$whereFrom-$whereTo;${moveNumber++}]"
 
     println(lineToSend)
 
     whereFrom = "D2"
     whereTo = "D3"
-    moveNumber = "№2"
-    lineToSend = """
-Следующий ход белых. Игрок сделал ход из $whereFrom в $whereTo $moveNumber
-"""
+    lineToSend = "[$whereFrom-$whereTo;${moveNumber++}]"
+
     println(lineToSend)
 
 }
