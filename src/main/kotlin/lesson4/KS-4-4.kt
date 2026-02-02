f250b274d25b2c60f1958e75fa629d38d3b60d9d
Андрей <minombre.andrei@yandex.ru>
2026-02-02 22:18:13 +0400

    KS-4-4

---
+++ src/main/kotlin/lesson4/KS-4-4.kt
@@ -0,0 +1,20 @@
+﻿package org.example.lesson4
+
+fun main() {
+    var trainingDay: Int = 5
+    val notEvenDay = trainingDay % 2 != 0
+
+    val exerciseForHands = notEvenDay
+    val exerciseForFoot = !notEvenDay
+    val exerciseForBack = !notEvenDay
+    val exerciseForPress = notEvenDay
+
+    println(
+        """
+        Упражнения для рук: $exerciseForHands
+        Упражнения для ног: $exerciseForFoot
+        Упражнения для спины: $exerciseForBack
+        Упражнения для пресса: $exerciseForPress
+    """.trimIndent()
+    )
+}
