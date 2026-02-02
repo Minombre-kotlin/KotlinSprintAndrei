efd476d0e1b35bf05f5272d2674140e6f5758ddf
Андрей <minombre.andrei@yandex.ru>
2026-02-02 22:12:57 +0400

    KS-4-3 fix

--- src/main/kotlin/lesson4/KS-4-3.kt
+++ src/main/kotlin/lesson4/KS-4-3.kt
@@ -1,11 +1,15 @@
 package org.example.lesson4
 
+const val SUNNY_WEATHER: Boolean = true
+const val OPEN_AWNING: Boolean = true
+const val AIR_HUMIDITY: Int = 20
+const val TIME_OF_YEAR: String = "Winter"
+
 fun main() {
-    val sunnyWeather: Boolean = true
-    val openAwning: Boolean = true
-    val airHumidity: Int = 20
-    val timeOfYear: String = "Winter"
-    val favorableConditions = sunnyWeather && openAwning && (airHumidity == 20) && (timeOfYear == "Winter")
+    var сurrentAirHumidity: Int = 20
+    var currentTimeOfYear: String = "Winter"
+    var favorableConditions =
+        SUNNY_WEATHER && OPEN_AWNING && (сurrentAirHumidity == AIR_HUMIDITY) && (currentTimeOfYear == TIME_OF_YEAR)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }
