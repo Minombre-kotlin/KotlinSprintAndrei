e60533f052ad08a00fdcc69cbafb11aa4e9d469f
Андрей <minombre.andrei@yandex.ru>
2026-02-06 16:27:04 +0400

    KS-5-1

---
+++ src/main/kotlin/lesson5/KS-5-1.kt
@@ -0,0 +1,17 @@
+﻿package org.example.lesson5
+
+fun main() {
+    val humanOrBot: String = "Пожалуйста, подтвердите, что вы не бот. Для этого напишите, сколько будет 2 + 2"
+    val correctAnswer: Int = 4
+    val successfulLogin: String = "Добро пожаловать!"
+    val accessDenied: String = "Доступ запрещен."
+
+    println(humanOrBot)
+    val answer = readln().toIntOrNull()
+
+    if (answer == correctAnswer) {
+        println(successfulLogin)
+    } else {
+        println(accessDenied)
+    }
+}
