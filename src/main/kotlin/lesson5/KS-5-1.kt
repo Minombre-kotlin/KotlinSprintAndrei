96c7611cc8cc1e22293ea66d8f7919b6e8eedae0
Андрей <minombre.andrei@yandex.ru>
2026-02-07 21:19:11 +0400

    KS-5-1

--- src/main/kotlin/lesson5/KS-5-1.kt
+++ src/main/kotlin/lesson5/KS-5-1.kt
@@ -1,7 +1,9 @@
 ﻿package org.example.lesson5
 
 fun main() {
-    val humanOrBot: String = "Пожалуйста, подтвердите, что вы не бот. Для этого напишите, сколько будет 2 + 2"
+    val firstNumber: Int = 2
+    val secondNumber: Int = 2
+    val humanOrBot: String = "Пожалуйста, подтвердите, что вы не бот. Для этого напишите, сколько будет $firstNumber + $secondNumber"
     val correctAnswer: Int = 4
     val successfulLogin: String = "Добро пожаловать!"
     val accessDenied: String = "Доступ запрещен."
