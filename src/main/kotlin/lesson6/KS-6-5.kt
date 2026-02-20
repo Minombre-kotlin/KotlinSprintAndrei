c7b417326d5c763b697fbbd02af256efa63c5864
andrei <minombre.andrei@yandex.ru>
2026-02-20 17:18:54 +0400

    KS-6-5 fix 2 Перенес генерацию чисел внутрь цикла do-while

--- src/main/kotlin/lesson6/KS-6-5.kt
+++ src/main/kotlin/lesson6/KS-6-5.kt
@@ -1,11 +1,11 @@
 package org.example.lesson6
 
 fun main() {
-    var number1 = (1..9).random()
-    var number2 = (1..9).random()
     var remainingAttempts = 3
 
     do {
+        var number1 = (1..9).random()
+        var number2 = (1..9).random()
         println("Для входа в приложение докажите, что вы не бот. Для этого решите пример и напишите ответ: $number1 + $number2")
         val userSum = readln().toInt()
         if (userSum == number1 + number2) {
