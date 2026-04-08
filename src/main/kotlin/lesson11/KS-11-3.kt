e05cd6d0baa67e572162830784022924247da790
andrei <minombre.andrei@yandex.ru>
2026-04-08 09:53:09 +0400

    KS-11-3 fix 2

--- src/main/kotlin/lesson11/KS-11-3.kt
+++ src/main/kotlin/lesson11/KS-11-3.kt
@@ -1,35 +1,28 @@
 package org.example.lesson11
 
 class User(
-
     val avatar: String,
     val nickname: String,
     var speakerStatus: String,
-
-    )
+)
 
 class Room(
-
     val cover: String,
     val name: String,
     val participantsList: MutableList<User> = mutableListOf(),
+) {
 
-    ) {
     fun addParticipantInRoom(user: User) {
-
         participantsList.add(user)
-
     }
 
     fun updateParticipantStatus(speakerStatus: String, nickname: String) {
-
         val user = participantsList.find { it.nickname == nickname }
         user?.speakerStatus = speakerStatus
     }
 }
 
 fun main() {
-
     val user1 = User(
         avatar = "панда",
         nickname = "Евгений",
