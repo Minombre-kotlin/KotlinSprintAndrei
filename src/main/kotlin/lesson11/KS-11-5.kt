c19eebea0a8051c697995037b04caf1ae0ae6d87
Андрей <minombre.andrei@yandex.ru>
2026-04-24 08:26:36 +0400

    KS-11-5 упростил решение в соответствии с замечаниями

--- src/main/kotlin/lesson11/KS-11-5.kt
+++ src/main/kotlin/lesson11/KS-11-5.kt
@@ -1,52 +1,14 @@
 package org.example.Lesson11
 
-class ForumMember private constructor(
-    val userID: Int,
+class ForumMember(
+    val userId: Int,
     val userName: String,
-) {
-    class Builder {
-        private var userID: Int = 0
-        private var userName: String = ""
+)
 
-        fun setUserID(id: Int): Builder {
-            this.userID = id
-            return this
-        }
-
-        fun setUserName(name: String): Builder {
-            this.userName = name
-            return this
-        }
-
-        fun build(): ForumMember {
-            return ForumMember(userID, userName)
-        }
-    }
-}
-
-class ForumMessage private constructor(
+class ForumMessage(
     val authorId: Int,
     val message: String,
-) {
-    class Builder {
-        private var authorId: Int = 0
-        private var message: String = ""
-
-        fun setAuthorId(authorId: Int): Builder {
-            this.authorId = authorId
-            return this
-        }
-
-        fun setMessage(message: String): Builder {
-            this.message = message
-            return this
-        }
-
-        fun build(): ForumMessage {
-            return ForumMessage(authorId, message)
-        }
-    }
-}
+)
 
 class Forum {
     private val members = mutableListOf<ForumMember>()
@@ -54,21 +16,14 @@ class Forum {
 
     fun createNewUser(userName: String): ForumMember {
         val newId = members.size + 1
-
-        val newMember = ForumMember.Builder()
-            .setUserID(newId)
-            .setUserName(userName)
-            .build()
+        val newMember = ForumMember(newId, userName)
         members.add(newMember)
         return newMember
     }
 
     fun createNewMessage(authorId: Int, message: String): ForumMessage {
-        if (members.any { it.userID == authorId }) {
-            val newMessage = ForumMessage.Builder()
-                .setAuthorId(authorId)
-                .setMessage(message)
-                .build()
+        if (members.any { it.userId == authorId }) {
+            val newMessage = ForumMessage(authorId, message)
             messages.add(newMessage)
             return newMessage
         } else {
@@ -78,9 +33,7 @@ class Forum {
 
     fun printThread() {
         messages.forEach { message ->
-            val author = members.find {
-                it.userID == message.authorId
-            }
+            val author = members.find { it.userId == message.authorId }
             println("${author?.userName}: ${message.message}")
         }
     }
@@ -92,10 +45,10 @@ fun main() {
     val user1 = forum.createNewUser("Евгений")
     val user2 = forum.createNewUser("Ольга")
 
-    forum.createNewMessage(user1.userID, "Привет!")
-    forum.createNewMessage(user2.userID, "Как дела?")
+    forum.createNewMessage(user1.userId, "Привет!")
+    forum.createNewMessage(user2.userId, "Как дела?")
+    forum.createNewMessage(user1.userId, "Всё хорошо!")
+    forum.createNewMessage(user2.userId, "Отлично")
 
-    forum.createNewMessage(user1.userID, "Привет!")
-    forum.createNewMessage(user2.userID, "Отлично")
     forum.printThread()
 }
