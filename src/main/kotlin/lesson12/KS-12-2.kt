b1c564f0dd69c12d801c3aee8e3963dc25cdb4d0
Андрей <minombre.andrei@yandex.ru>
2026-05-14 08:31:02 +0400

    KS-12-2

--- src/main/kotlin/lesson12/KS-12-2.kt
+++ src/main/kotlin/lesson12/KS-12-2.kt
@@ -1,20 +1,15 @@
 package org.example.lesson12
 
-class Weather2(dayKelvin: Int, nightKelvin: Int) {
-    var dayTemp: Int
-    var nightTemp: Int
-
-    init {
-        dayTemp = dayKelvin - 273
-        nightTemp = nightKelvin - 273
-    }
+class WeatherInfo(val dayTemp: Double, val nightTemp: Double, val hasRain: Boolean) {
 
     fun printInfo() {
-        println("День: $dayTemp°C, Ночь: $nightTemp°C")
+        println("День: $dayTemp°C, Ночь: $nightTemp°C, Осадки: $hasRain")
     }
 }
 
 fun main() {
-    val weather = Weather2(300, 290)
+
+    val weather = WeatherInfo(20.0, 17.0, false)
     weather.printInfo()
+
 }
