ab220f6759c456fb3b6b4c3afebe52d271c6a811
andrei <minombre.andrei@yandex.ru>
2026-05-28 10:11:53 +0400

    KS-13-2 fix

--- src/main/kotlin/lesson13/KS-13-2.kt
+++ src/main/kotlin/lesson13/KS-13-2.kt
@@ -1,21 +1,20 @@
 package org.example.lesson13
 
-class TelephoneDirectoryNew(
+class Contact(
     val name: String,
     val phoneNumber: Long,
     val company: String? = null,
-
 ) {
     fun printContact() {
         val companyDisplay = company ?: "<не указано>"
-        println("Имя: $name, Номер: $phoneNumber, Компания: $companyDisplay")
+        println("Имя:$name\nНомер:$phoneNumber\nКомпания:$companyDisplay")
     }
 }
 
 fun main() {
-    val contact1 = TelephoneDirectoryNew("Анна", 89501234567, "Аэрофлот")
-    val contact2 = TelephoneDirectoryNew("Тимофей", 89511234567, null)
-    val contact3 = TelephoneDirectoryNew("Иван", 89123456789)
+    val contact1 = Contact("Анна", 89501234567, "Аэрофлот")
+    val contact2 = Contact("Тимофей", 89511234567, null)
+    val contact3 = Contact("Иван", 89123456789)
 
     contact1.printContact()
     contact2.printContact()
